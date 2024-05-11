4112197c5e9bca64af77f5b6ba565977b96064b6
creynolds8 <cameron8.cr@gmail.com>
2024-05-10 17:07:37 -0700

    create subclasses

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 let balance = 500.00;
+const myAccount = new Account('13hours')
 
-class Deposit {
-  constructor(amount) {
-    this.amount = amount;
-  };
-  commit() {
-    balance += this.amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   };
 };
 
-class Withdrawal {
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   };
   commit() {
-    balance -= this.amount;
+    this.amount += this.value
+  }
+
+}
+
+class Deposit extends Transaction {
+   get commit() {
+    return this.amount;
+  };
+};
+
+class Withdrawal extends Transaction {
+  get value() {
+      return -this.amount;
   };
 };
 
@@ -24,15 +37,17 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00);
+t3 = new Deposit(120.00, myAccount);
 t3.commit();
 console.log('Transaction 3:', t3);
 
